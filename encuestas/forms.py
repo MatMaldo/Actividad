e6875ebe817df31489087e27bd132d378029307e
Jeremias Alfaro <jere.alfaro@duocuc.cl>
2024-06-06 22:06:12 -0400

    Se agrega class en forms para el registro de usuarios

--- encuestas/forms.py
+++ encuestas/forms.py
@@ -2,9 +2,18 @@ from django import forms
 from .models import Pregunta
 from django.forms import ModelForm
 
+#para el registro de usuarios
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 class PreguntaForm(ModelForm):
     class Meta:
         model = Pregunta
         fields = "__all__"
         # fields = ["pregunta_text", "fecha_pub"]
         widgets = { 'fecha_pub' : forms.DateTimeInput( attrs = {'type':'datetime-local'} )}
+
+class PruebaUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
